30632014c9654eecad7c05fef535507bca5cae47
Grigory Simeonov <sgmdoc@yandex.ru>
2020-10-25 14:01:02 +0500

    fix: add popup input values

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
 'use strict';
 
-//!Открытие попапа
+//! Объявление переменных
+//? Я решил все переменные вынести из своих блоков и объявить одной группой, правильно ли это?
+//? Или лучше каждую группу переменных объявлять радом с блоком, где они используются?
+
+//Попап
 let profile = document.querySelector('.profile');
 let popup = document.querySelector('.popup');
 
 let profileEdit = profile.querySelector('.profile__edit-button');
 let popupClose = popup.querySelector('.popup__close-button');
 
+//Редактирование
+let formElement = popup.querySelector('.popup__container');
+let nameInput = popup.querySelector('.popup__input_name');
+let jobInput = popup.querySelector('.popup__input_occupation');
+
+let profileName = profile.querySelector('.profile__name');
+let profileOccupation = profile.querySelector('.profile__occupation');
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileOccupation.textContent;
+
+//! Функция открытия попапа
 function popupOpened() {
   popup.classList.add('popup_opened');
 }
@@ -15,7 +31,8 @@ profileEdit.addEventListener('click', popupOpened);
 
 function popupClosed() {
   popup.classList.remove('popup_opened');
-  formElement.reset();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileOccupation.textContent;
 }
 
 popupClose.addEventListener('click', popupClosed);
@@ -28,13 +45,7 @@ function popupWindowClose(event) {
 
 popup.addEventListener('mousedown', popupWindowClose);
 
-//!Редактирование профиля
-let formElement = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('.popup__input_name');
-let jobInput = popup.querySelector('.popup__input_occupation');
-
-let profileName = profile.querySelector('.profile__name');
-let profileOccupation = profile.querySelector('.profile__occupation');
+//! Функция редактирования профиля
 
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -45,11 +56,12 @@ function formSubmitHandler(event) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-//! Закрытие по нажатию Escape
+//! Функция закрытия окна по нажатию Escape
 function formSubmitClose(event) {
   if (event.key === 'Escape') {
     popupClosed();
-    formElement.reset();
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileOccupation.textContent;
   }
 }
 
